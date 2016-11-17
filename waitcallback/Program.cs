2c77b7227a76f47a9bf2834c68231e198c707eeb
zohal <zohal@DESKTOP-9RC0L6A>
2016-11-17 12:38:23 -0500

    Add project files.

---
+++ waitcallback/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace waitcallback
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ThreadPool.QueueUserWorkItem(ThreadProc);
+            Console.WriteLine("Main Thread does some work, then sleeps");
+            Thread.Sleep(1000);
+            Console.WriteLine("Main thread exits");
+        }
+        private static void ThreadProc (Object StateInfo) {
+            Console.WriteLine("Printing from the spunned thread");
+            Console.ReadLine();
+        }
+    }
+}
